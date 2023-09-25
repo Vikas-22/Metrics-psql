a38a763c0b001afa02c0ca2997063a35062d7a03
Vikas-22 <98518913+Vikas-22@users.noreply.github.com>
2023-09-25 17:52:58 +0530

    Add files via upload

---
+++ Metrices-psql/Models/Metric.cs
@@ -0,0 +1,11 @@
+﻿namespace Metrices_psql.Models
+{
+    public class Metric
+    {
+        public int Id { get; set; }
+        public string MetricName { get; set; }
+        public int MetricValue { get; set; }
+        public DateTime MetricTimestamp { get; set; } = DateTime.Now.ToUniversalTime();
+
+    }
+}
