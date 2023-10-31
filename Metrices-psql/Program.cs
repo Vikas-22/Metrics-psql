610945c1b66f81f1b42c1bf6c1efbd7e5f5fbcda
vg3 <vikas.a.g@capgemini.com>
2023-10-31 15:21:02 +0530

    Startup Added

--- Metrices-psql/Program.cs
+++ Metrices-psql/Program.cs
@@ -8,41 +8,48 @@ using Metrices_psql;
 using Metrices_psql.datalayer;
 using Metrices_psql.Controllers;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using Metrices_psql;
 
-
-//builder.Services.AddScoped<PrometheusQueryService>();   
-builder.Services.AddScoped<IEmployesRepository,EmployesRepository>();
-builder.Services.AddScoped<IPrometheusQueryServices, PrometheusQueryService>();
-builder.Services.AddHttpClient<PrometheusQueryService>();
-
-// Configure database context
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
 
-app.MapControllers();
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//var app = builder.Build();
 
 
-// Configure Prometheus metrics
-app.UseMetricServer();
-app.UseHttpMetrics();
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-app.Run();
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+
+//// Configure Prometheus metrics
+//app.UseMetricServer();
+//app.UseHttpMetrics();
+
+//app.Run();
